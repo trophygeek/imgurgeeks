c85743c3ec7bba2e454f07b4782f0bc5862daefa
trophygeek <trophygeek@gmail.com>
2020-11-08 09:45:00 -0800

    - Make work with `persistent: false` in manifest. - Incr version

--- imgurgeeks-extention/src/menu.js
+++ imgurgeeks-extention/src/menu.js
@@ -2,21 +2,25 @@
 
 try {
   document.getElementById('post_stats').addEventListener('click', (evt) => {
+    // make sure the background task is running before opening page.
+    chrome.runtime.getBackgroundPage(function (backgroundPage) {
       chrome.tabs.create({
         url: "https://imgur.com/a/tponNW4"
       });
       window.close();
-  });
-
-
-  document.getElementById('options').addEventListener('click', (evt) =>  {
-    const id = chrome.runtime.id;
-    chrome.tabs.create({
-      url: `chrome-extension://${id}/src/options.html`
     });
-    window.close();
   });
 
+
+  document.getElementById('options').addEventListener('click', (evt) => {
+    chrome.runtime.getBackgroundPage(function (backgroundPage) {
+      const id = chrome.runtime.id;
+      chrome.tabs.create({
+        url: `chrome-extension://${id}/src/options.html`
+      });
+      window.close();
+    });
+  });
 } catch (err) {
   console.log(err);
 }
